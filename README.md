1f68c5165b81f60a9e6c4bdec5bfe53e1ad11ab6
Mike Bell <mdb036@gmail.com>
2024-07-27 20:57:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tiny Tapeout 04+ demo board C example
+
+This firmware using the Pico C SDK gives an example of how to bring up the TT ASIC.
+
+See the Raspberry Pi [Getting Started with Pico](https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf) guide for information on how to get set up to build the firmware.
---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <pico/stdlib.h>
+#include <hardware/sync.h>
+#include <pico/multicore.h>
+
+#include <array>
+#include <cstdio>
+
+#include "tt_setup.h"
+#include "tt_pins.h"
+
+#define DESIGN_NUM 1
+
+int main() {
+    set_sys_clock_khz(133000, true);
+
+    stdio_init_all();
+
+    // Uncomment to pause until the USB is connected before continuing
+    //while (!stdio_usb_connected());
+
+    sleep_ms(20);
+    printf("Selecting Factory Test\n");
+    sleep_ms(10);
+
+    tt_select_design(DESIGN_NUM);
+    printf("Selected\n");
+    sleep_ms(10);
+
+    // Clock in reset and then take out of reset
+    tt_clock_project_once();
+    gpio_put(SDI_nRST, 1);
+
+    // Factory test counts if in0 is high
+    gpio_put(IN0, 1);
+
+    while (1) {
+        tt_clock_project_once();
+        sleep_ms(100);
+    }
+}
