f0acfef3e95ce0d0acc418da854d58e3453beb15
Mike Bell <mdb036@gmail.com>
2024-07-28 01:10:41 +0100

    Controls

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ See the Raspberry Pi [Getting Started with Pico](https://datasheets.raspberrypi.
 Note you must build in Release mode.
 
 This project overclocks the RP2040 to 300MHz and overvolts it to 1.2V (0.1V above stock) to ensure that is stable.  This may void your warranty, use at your own risk.
+
+## Controls
+
+On boot, the RP2040 waits for either one of the 5 input buttons to be pressed, or a connection to USB serial.
+
+If a USB connection is made then the RP2040 drives the inputs, the controls are:
+| Key | Button |
+| --- | ------ |
+| a   | Left   |
+| d   | Right  |
+| s   | Drop   |
+| w or q | Rotate anticlockwise |
+| e | Rotate clockwise |
+
+If a button is pressed, then the RP2040 starts the game without taking control of the input pins.
--- main.cpp
+++ main.cpp
@@ -23,6 +23,8 @@
 
 #define DIVIDER 48
 
+static  uint8_t memory[64][16] = { 0 };
+
 static void __not_in_flash_func(core1_main)() {
     // Start 6.25MHz clock = 200/32
     gpio_set_function(0, GPIO_FUNC_PWM);
@@ -34,17 +36,17 @@ static void __not_in_flash_func(core1_main)() {
     // Set the PWM running
     pwm_set_enabled(0, true);
 
-  uint8_t memory[64][16] = { 0 };
   uint8_t addr_hi = 0;
   uint8_t addr_lo = 0;
 
   memset(memory, 0x0, 64 * 16);
 
+  /*
   for (int i = 0; i < 64; ++i) {
     memory[i][0] = 0x4;
     memory[i][1] = 0x5;
     memory[i][2] = 0x6;
-  }
+  }*/
 
   /*memory[0][5] = 0x0;
   memory[10][5] = 0x0;*/
@@ -151,123 +153,6 @@ static void __not_in_flash_func(core1_main)() {
 
 }
 
-const uint CAPTURE_PIN_BASE = 14;
-const uint CAPTURE_PIN_COUNT = 15;
-const uint CAPTURE_N_SAMPLES = 200;
-
-static inline uint bits_packed_per_word(uint pin_count) {
-    // If the number of pins to be sampled divides the shift register size, we
-    // can use the full SR and FIFO width, and push when the input shift count
-    // exactly reaches 32. If not, we have to push earlier, so we use the FIFO
-    // a little less efficiently.
-    const uint SHIFT_REG_WIDTH = 32;
-    return SHIFT_REG_WIDTH - (SHIFT_REG_WIDTH % pin_count);
-}
-
-void logic_analyser_init(PIO pio, uint sm, uint pin_base, uint pin_count, float div) {
-    // Load a program to capture n pins. This is just a single `in pins, n`
-    // instruction with a wrap.
-    uint16_t capture_prog_instr = pio_encode_in(pio_pins, pin_count);
-    struct pio_program capture_prog = {
-            .instructions = &capture_prog_instr,
-            .length = 1,
-            .origin = -1
-    };
-    uint offset = pio_add_program(pio, &capture_prog);
-
-    // Configure state machine to loop over this `in` instruction forever,
-    // with autopush enabled.
-    pio_sm_config c = pio_get_default_sm_config();
-    sm_config_set_in_pins(&c, pin_base);
-    sm_config_set_wrap(&c, offset, offset);
-    sm_config_set_clkdiv(&c, div);
-    // Note that we may push at a < 32 bit threshold if pin_count does not
-    // divide 32. We are using shift-to-right, so the sample data ends up
-    // left-justified in the FIFO in this case, with some zeroes at the LSBs.
-    sm_config_set_in_shift(&c, true, true, bits_packed_per_word(pin_count));
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
-    pio_sm_init(pio, sm, offset, &c);
-}
-
-void logic_analyser_arm(PIO pio, uint sm, uint dma_chan, uint32_t *capture_buf, size_t capture_size_words,
-                        uint trigger_pin, bool trigger_level) {
-    pio_sm_set_enabled(pio, sm, false);
-    // Need to clear _input shift counter_, as well as FIFO, because there may be
-    // partial ISR contents left over from a previous run. sm_restart does this.
-    pio_sm_clear_fifos(pio, sm);
-    pio_sm_restart(pio, sm);
-
-    dma_channel_config c = dma_channel_get_default_config(dma_chan);
-    channel_config_set_read_increment(&c, false);
-    channel_config_set_write_increment(&c, true);
-    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
-
-    dma_channel_configure(dma_chan, &c,
-        capture_buf,        // Destination pointer
-        &pio->rxf[sm],      // Source pointer
-        capture_size_words, // Number of transfers
-        true                // Start immediately
-    );
-
-    pio_sm_exec(pio, sm, pio_encode_wait_gpio(trigger_level, trigger_pin));
-    pio_sm_set_enabled(pio, sm, true);
-}
-
-void print_capture_buf(const uint32_t *buf, uint pin_base, uint pin_count, uint32_t n_samples) {
-    // Display the capture buffer in text form, like this:
-    // 00: __--__--__--__--__--__--
-    // 01: ____----____----____----
-    printf("Capture:\n");
-    // Each FIFO record may be only partially filled with bits, depending on
-    // whether pin_count is a factor of 32.
-    uint record_size_bits = bits_packed_per_word(pin_count);
-    for (uint pin = 0; pin < pin_count; ++pin) {
-        printf("%02d: ", pin + pin_base);
-        for (uint32_t sample = 0; sample < n_samples; ++sample) {
-            uint bit_index = pin + sample * pin_count;
-            uint word_index = bit_index / record_size_bits;
-            // Data is left-justified in each FIFO entry, hence the (32 - record_size_bits) offset
-            uint word_mask = 1u << (bit_index % record_size_bits + 32 - record_size_bits);
-            printf(buf[word_index] & word_mask ? "-" : "_");
-        }
-        printf("\n");
-    }
-}
-
-void capture() {
-
-    // We're going to capture into a u32 buffer, for best DMA efficiency. Need
-    // to be careful of rounding in case the number of pins being sampled
-    // isn't a power of 2.
-    uint total_sample_bits = CAPTURE_N_SAMPLES * CAPTURE_PIN_COUNT;
-    total_sample_bits += bits_packed_per_word(CAPTURE_PIN_COUNT) - 1;
-    uint buf_size_words = total_sample_bits / bits_packed_per_word(CAPTURE_PIN_COUNT);
-    uint32_t *capture_buf = (uint32_t*)malloc(buf_size_words * sizeof(uint32_t));
-    hard_assert(capture_buf);
-
-    // Grant high bus priority to the DMA, so it can shove the processors out
-    // of the way. This should only be needed if you are pushing things up to
-    // >16bits/clk here, i.e. if you need to saturate the bus completely.
-    //bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
-
-    PIO pio = pio0;
-    uint sm = 0;
-    uint dma_chan = 0;
-
-    logic_analyser_init(pio, sm, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, DIVIDER/2);
-
-    printf("Arming trigger\n");
-    logic_analyser_arm(pio, sm, dma_chan, capture_buf, buf_size_words, 28, false);
-
-    multicore_launch_core1(core1_main);
-
-    // The logic analyser should have started capturing as soon as it saw the
-    // first transition. Wait until the last sample comes in from the DMA.
-    dma_channel_wait_for_finish_blocking(dma_chan);
-
-    print_capture_buf(capture_buf, CAPTURE_PIN_BASE, CAPTURE_PIN_COUNT, CAPTURE_N_SAMPLES);
-}
-
 int main() {
     vreg_set_voltage(VREG_VOLTAGE_1_20);
 	sleep_ms(10);
@@ -276,8 +161,15 @@ int main() {
 
     stdio_init_all();
 
-    // Uncomment to pause until the USB is connected before continuing
-    //while (!stdio_usb_connected());
+    // If a button is pressed while waiting for USB connection,
+    // then don't take control of inputs
+    bool usb_control = true;
+    while (!stdio_usb_connected()) {
+        if (gpio_get_all() & ((1 << IN4) | (0xF << IN0))) {
+            usb_control = false;
+            break;
+        }
+    }
 
     sleep_ms(20);
     printf("Selecting Tetris\n");
@@ -296,7 +188,23 @@ int main() {
     // Run on core 1 to avoid interrupts as timing is critical
     multicore_launch_core1(core1_main);
 
-    //capture();
+    if (usb_control) {
+        gpio_set_dir_out_masked((1 << IN4) | (0xF << IN0));
+        while (1) {
+            int c = getchar();
+            switch (c) {
+                case 'a': tt_set_input_byte(1); break;
+                case 's': tt_set_input_byte(4); break;
+                case 'd': tt_set_input_byte(2); break;
+                case 'q': case 'w': tt_set_input_byte(8); break;
+                case 'e': tt_set_input_byte(16); break;
+            }
+            sleep_us(2000);
+            tt_set_input_byte(0);
+        }
+    }
+    else {
+        while (1) __wfe();
+    }
 
-    while (1) __wfe();
 }
