8779b9f3811fb57de4e5c7d6a6d59c05880f8827
Mike Bell <mdb036@gmail.com>
2025-02-01 17:17:03 +0000

    TT06

--- tt_pins.h
+++ tt_pins.h
@@ -2,28 +2,29 @@
 
 #include "hardware/gpio.h"
 
+// Mapping for PGA2350-TT06 with broken TT05 carrier converter
 enum GPIOMap {
     CLK = 0,
-    HK_CSB = 1,
-    HK_SCK = 2,
-    SDI_nRST = 3,
-    SDO = 4,
-    OUT0 = 5,
-    CTRL_ENA_OUT1 = 6,
-    nCRST_OUT2 = 7,
-    CINC_OUT3 = 8,
+    nRST = 1,
+    CENA = 4,
+    nCRST = 2,
+    CINC = 3,
     IN0 = 9,
     IN1 = 10,
     IN2 = 11,
     IN3 = 12,
-    OUT4 = 13,
-    OUT5 = 14,
-    OUT6 = 15,
-    OUT7 = 16,
     IN4  = 17,
     IN5  = 18,
     IN6  = 19,
     IN7  = 20,
+    OUT0 = 5,
+    OUT1 = 6,
+    OUT2 = 7,
+    OUT3 = 8,
+    OUT4 = 13,
+    OUT5 = 14,
+    OUT6 = 15,
+    OUT7 = 16,
     UIO0 = 21,
     UIO1 = 22,
     UIO2 = 23,
@@ -32,7 +33,6 @@ enum GPIOMap {
     UIO5 = 26,
     UIO6 = 27,
     UIO7 = 28,
-    RPIO29 = 29,
 };
 
 inline static void tt_set_input_byte(int val) {
